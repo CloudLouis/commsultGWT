c7527eafe1ef0830a426d035826157497057dd11
User One <user.one@email.com>
2018-10-08 11:48:26 +0700

    Removed .gitignore for push

--- src/main/java/com/commsult/project/client/CommsultGWT.java
+++ src/main/java/com/commsult/project/client/CommsultGWT.java
@@ -6,8 +6,10 @@ import com.commsult.project.server.MainController;
 import com.commsult.project.server.Sensors;
 import com.commsult.project.server.Thermometer;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,18 +17,24 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class CommsultGWT implements EntryPoint {
 	
 	  private Label labelPrint = new Label();
+	  private HorizontalPanel valuePanel = new HorizontalPanel();
+	  private Label tempLabel = new Label();
+	  private Label timeLabel = new Label();
+	  private Label windLabel = new Label();
+	  private VerticalPanel mainPanel = new VerticalPanel();	
 
 	public void onModuleLoad() {
 		MainController controllerObserver = new MainController();
 		ArrayList<Sensors> sensors = new ArrayList<>();
 		sensors.add(new Thermometer(controllerObserver));
 		sensors.get(0).setMeasurement(10.00);
-		
+		valuePanel.add(tempLabel);
+		valuePanel.add(timeLabel);
+		valuePanel.add(windLabel);
 	}
 	
 	public void printSomething(String toprint) {
 		labelPrint.setText(toprint);
-		RootPanel.get("testprint").add(labelPrint);
 	}
 
 	
